7d1a6c864282ba594814c87004c65343aa2ee78e
Andréia/Saria <63603210+AndreiaSaria@users.noreply.github.com>
2020-06-03 09:43:21 -0300

    Added number text to check HP

--- Assets/Scripts/HPCount.cs
+++ Assets/Scripts/HPCount.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using TMPro;
 
 public class HPCount : MonoBehaviour
 {
@@ -9,10 +10,12 @@ public class HPCount : MonoBehaviour
     public int HP;
     //colei daqui https://answers.unity.com/questions/1633697/cant-drag-slider-onto-inspector.html
     public UnityEngine.UI.Slider sliderHp;
+    public TextMeshProUGUI HpText;
 
     private void OnBecameVisible()
     {
         sliderHp.value = HP;
+        HpText.SetText(HP.ToString());
     }
 
     public void Damaged(int i) //Desse jeito que fiz não dá para ser estático. That just happened.
@@ -21,11 +24,13 @@ public class HPCount : MonoBehaviour
         HP = HP - i;
         //Debug.Log("Hp dropped " + i + "values");
         sliderHp.value = HP;
+        HpText.SetText(HP.ToString());
 
         if (HP <= 0)
         {
             SendMessage("Death");
             sliderHp.gameObject.SetActive(false);
+            HpText.SetText("Dead");
 
             if (this.gameObject.tag != "Player")
             {
